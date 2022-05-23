e6d2d4c04a6704c5c83714cac570e58d983de563
colour-me-bibi <bennyforeman1@gmail.com>
2022-05-23 16:09:02 -0700

    Implemented recursive solution

--- 99_bottles.js
+++ 99_bottles.js
@@ -1,4 +1,15 @@
 /**
+ * This function returns the pluralized version of the word if n is not 1.
+ *
+ * @param {Integer} n The number of items
+ * @param {String} word The word to pluralize
+ * @returns
+ */
+function pluralize(n, word) {
+  return `${n} ${word}${n == 1 ? "" : "s"}`;
+}
+
+/**
  * This function sings the bottle song by printing to the console:
  *    99 bottles of beer on the wall, 99 bottles of beer.
  *    Take one down and pass it around, 98 bottles of beer on the wall.
@@ -26,24 +37,41 @@
  *    Take one down and pass it around, no more bottles of beer on the wall.
  *    No more bottles of beer on the wall, no more bottles of beer.
  *    Go to the store and buy some more, 99 bottles of beer on the wall.
- *
- * @param {Integer} start The starting number of bottles
  */
-function bottleSong(start) {
-  for (let n = start; n > 0; n--) {
+function bottleSong() {
+  for (let nBottles = 99; nBottles > 0; nBottles--) {
     console.log(
-      `${n} bottle${n == 1 ? "" : "s"} of beer on the wall, ` +
-        `${n} bottle${n == 1 ? "" : "s"} of beer.\n` +
-        "Take one down and pass it around, " +
-        `${n - 1} bottle${n - 1 == 1 ? "" : "s"} of beer on the wall.`
+      `${pluralize(nBottles, "bottle")} of beer on the wall, ${pluralize(nBottles, "bottle")} of beer.\n` + 
+      `Take one down and pass it around, ${pluralize(nBottles - 1, "bottle")} of beer on the wall.`
     );
   }
 
   console.log(
     "No more bottles of beer on the wall, no more bottles of beer.\n" +
-      "Go to the store and buy some more, " +
-      `${start} bottle${start == 1 ? "" : "s"} of beer on the wall.`
+    "Go to the store and buy some more, 99 bottles of beer on the wall."
   );
 }
 
-bottleSong(99);
+/**
+ * This function recursively sings the bottles song.
+ *
+ * @param {Integer} nBottles The number of bottles to sing
+ */
+function recursiveBottleSong(nBottles = 99) {
+  if (nBottles === 0) {
+    console.log(
+      "No more bottles of beer on the wall, no more bottles of beer.\n" +
+      "Go to the store and buy some more, 99 bottles of beer on the wall."
+    );
+    return;
+  }
+
+  console.log(
+    `${pluralize(nBottles, "bottle")} of beer on the wall, ${pluralize(nBottles, "bottle")} of beer.\n` +
+    `Take one down and pass it around, ${pluralize(nBottles - 1, "bottle")} of beer on the wall.`
+  );
+  recursiveBottleSong(nBottles - 1);
+}
+
+bottleSong();
+// recursiveBottleSong();
